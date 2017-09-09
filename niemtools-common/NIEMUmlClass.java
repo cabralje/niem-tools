4c724037f1a73034d9bb9130e2089b9853e253af
James E. Cabral Jr. <jim@cabral.org>
2017-09-09 09:55:08 -0400

    Added GNU GPL license as required by BOUML

--- niemtools-common/NIEMUmlClass.java
+++ niemtools-common/NIEMUmlClass.java
@@ -1,12 +1,25 @@
-// James E. Cabral Jr, MTG Management Consultants LLC jcabral@mtgmc.com, https://github.com/cabralje/niemtools
-// Original version:  9/24/15
-// Current version: 1/7/2017
-
-// This is a plug_out that extends the BOUML UML tool with support for the National Information Exchange Model (NIEM) v3.2 defined at http://niem.gov.
-// Specifically, it enables a UML Common Information Model (CIM), an abstract class mode, to be mapped into a
-// UML Platform Specific Model (PSM), the NIEM reference/subset/extension model, and a UML Platform Specific Model (NIEM), NIEM XML Schema.
-//
-// NOTE: This plug_out requires that the BOUML project include a simple NIEM profile that provides the stereotypes required for mapping.
+/*
+ *   NIEMtools - This is a plug_out that extends the BOUML UML tool with support for the National Information Exchange Model (NIEM) defined at http://niem.gov.
+ *   Specifically, it enables a UML Common Information Model (CIM), an abstract class mode, to be mapped into a
+ *   UML Platform Specific Model (PSM), the NIEM reference/subset/extension model, and a UML Platform Specific Model (NIEM), NIEM XML Schema.
+ *
+ *   NOTE: This plug_out requires that the BOUML project include a simple NIEM profile that provides the stereotypes required for mapping.
+ *   
+ *   Copyright (C) 2017 James E. Cabral Jr., MTG Management Consultants LLC
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import java.io.File;
 import java.io.FileNotFoundException;
